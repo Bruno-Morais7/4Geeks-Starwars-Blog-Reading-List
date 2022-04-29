c3225457c25111ec513b5378c2c7c214eb241a5f
Bruno-Morais7 <brunomafonsomorais@gmail.com>
2022-04-29 16:07:26 +0000

    Work so far, no logic at all

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import starWars from "../../img/brandSW.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <div className="container-fluid col-8">
+        <Link to="/">
+          <span className="navbar-brand">
+            <img src={starWars} width="125px"></img>
+          </span>
+        </Link>
+        <div className="dropdown">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Favorites 
+			<span class="badge badge-primary badge-pill">{"counterFavorites"}</span>
+          </button>
+          {/* <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+          </div> */}
+        </div>
+        {/* <Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+				</Link> */}
+      </div>
+    </nav>
+  );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,162 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import DumImg from "../../img/DumImg.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+  <div className="container-fluid col-8 mt-5">
+    <h2 className="text-danger">Characters</h2>
+	
+	<div>
+    <div className="justify-content-start d-flex styles="data-spy="scroll">
+      
+	  <div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">character.name</h5>
+          <p className="card-text mb-1">char.gender</p>
+          <p className="card-text mb-1">char.Hair-Color</p>
+          <p className="card-text mb-1">char.Eye-Color</p>
+          <div className="d-flex justify-content-between mt-3">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+	  <div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">character.name</h5>
+          <p className="card-text mb-1">char.gender</p>
+          <p className="card-text mb-1">char.Hair-Color</p>
+          <p className="card-text mb-1">char.Eye-Color</p>
+          <div className="d-flex justify-content-between">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+	  <div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">character.name</h5>
+          <p className="card-text mb-1">char.gender</p>
+          <p className="card-text mb-1">char.Hair-Color</p>
+          <p className="card-text mb-1">char.Eye-Color</p>
+          <div className="d-flex justify-content-between">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+	  <div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">character.name</h5>
+          <p className="card-text mb-1">char.gender</p>
+          <p className="card-text mb-1">char.Hair-Color</p>
+          <p className="card-text mb-1">char.Eye-Color</p>
+          <div className="d-flex justify-content-between">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+	  <div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">character.name</h5>
+          <p className="card-text mb-1">char.gender</p>
+          <p className="card-text mb-1">char.Hair-Color</p>
+          <p className="card-text mb-1">char.Eye-Color</p>
+          <div className="d-flex justify-content-between">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+	  <div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">character.name</h5>
+          <p className="card-text mb-1">char.gender</p>
+          <p className="card-text mb-1">char.Hair-Color</p>
+          <p className="card-text mb-1">char.Eye-Color</p>
+          <div className="d-flex justify-content-between">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+	  <div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">character.name</h5>
+          <p className="card-text mb-1">char.gender</p>
+          <p className="card-text mb-1">char.Hair-Color</p>
+          <p className="card-text mb-1">char.Eye-Color</p>
+          <div className="d-flex justify-content-between">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+	  </div>
+
+	  <h2 className="text-danger mt-5">Planets</h2>
+
+    <div className="justify-content-start d-flex styles="data-spy="scroll"></div>
+	<div className="card w-25">
+        <img className="card-img-top" src={DumImg} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">planet.name</h5>
+          <p className="card-text mb-1">planet.pop</p>
+          <p className="card-text mb-1">planet.terrain</p>
+          <div className="d-flex justify-content-between mt-3">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+            <button className="btn btn-outline-warning ml-auto align-self-center">
+              <i className="fa-regular fa-heart text-warning"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+ 
 );
